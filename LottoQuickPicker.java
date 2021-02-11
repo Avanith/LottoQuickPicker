1c8b0d8a2fcb955c661b371773870de1d96fee41
Daniel Targonski <daniel.targonski2@gmail.com>
2021-02-11 18:56:07 -0500

    formatting

--- LottoQuickPicker.java
+++ LottoQuickPicker.java
@@ -7,195 +7,193 @@ import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Date;
 
-import javax.swing.JOptionPane;
-
 import edu.cuny.csi.csc330.util.Randomizer;
 
 public class LottoQuickPicker {
-	
-	// constants  specific to current game - BUT NOT ALL GAMES 
-	public final static int DEFAULT_GAME_COUNT = 1; 
+
+	// constants specific to current game - BUT NOT ALL GAMES
+	public final static int DEFAULT_GAME_COUNT = 1;
 	private static int GAME_COUNT = 1;
-	private final static String GAME_NAME = "LOTTO"; 
-	private final static int SELECTION_POOL_SIZE = 59; 
-	private final static int SELECTION_COUNT = 6; 
+	private final static String GAME_NAME = "LOTTO";
+	private final static int SELECTION_POOL_SIZE = 59;
+	private final static int SELECTION_COUNT = 6;
 	private final static String STORE_NAME = "Dan's Deli";
-	private int[][] lottoArray = new int[GAME_COUNT][SELECTION_COUNT]; 
-
-
-/** 
- * Default Constructor that makes a single game.
- */	
-	public LottoQuickPicker() {
-		init(DEFAULT_GAME_COUNT); 
-	} // END LOTTOQUICKPICKER
-
-/** 
- * Constructor that creates "games" number of games.
- */
-	public LottoQuickPicker(int games) {
-		GAME_COUNT = games;
-		init(games); 
-	} // end LOTTOQUICKPICKER
-  
+	private int lottoArray[][] = new int[GAME_COUNT][SELECTION_COUNT];
 
 	/**
-	 * This array will call on two helper methods to generate a 2-D array
-	 * with each row having unique numbers.
+	 * Default Constructor that makes a single game.
+	 */
+	public LottoQuickPicker() {
+		init(DEFAULT_GAME_COUNT);
+	} // END LOTTOQUICKPICKER
+
+	/**
+	 * Constructor that creates "games" number of games.
+	 */
+	public LottoQuickPicker(int games) {
+		GAME_COUNT = games;
+		init(games);
+	} // end LOTTOQUICKPICKER
+
+	/**
+	 * This array will call on two helper methods to generate a 2-D array with each
+	 * row having unique numbers.
+	 * 
 	 * @return A 2-D array that has unique numbers for each of its rows.
 	 */
 	public int[][] uniqueNumArray() {
 		int[][] uniqueArray = new int[GAME_COUNT][SELECTION_COUNT];
-		
-		for (int i = 0; i < GAME_COUNT; i++) 
-				uniqueArray[i] = generateUniqueArray();
+
+		for (int i = 0; i < GAME_COUNT; i++)
+			uniqueArray[i] = generateUniqueArray();
 
 		return uniqueArray;
 	} // END UNIQUENUMARRAY
-	
-	/** Helper Method
-	 * Generates a 1-D array with unique numbers from 1 - selection pool size.
+
+	/**
+	 * Helper Method Generates a 1-D array with unique numbers from 1 - selection
+	 * pool size.
+	 * 
 	 * @return A 1-D array with unique numbers.
 	 */
-	private static int[] generateUniqueArray () {
+	private static int[] generateUniqueArray() {
 		int[] array = new int[SELECTION_COUNT];
 		int temp;
 
 		for (int i = 0; i < array.length; i++) {
-			temp = Randomizer.generateInt(1,SELECTION_POOL_SIZE);
+			temp = Randomizer.generateInt(1, SELECTION_POOL_SIZE);
 			// if the array doesn't have the number, then add the number to the array.
 			// can write separate method for this
 			while (arrayHasNumber(temp, array)) {
-				temp = Randomizer.generateInt(1,SELECTION_POOL_SIZE);
+				temp = Randomizer.generateInt(1, SELECTION_POOL_SIZE);
 			} // END while statement
 
 			array[i] = temp;
 		} // end for-loop
-		
+
 		Arrays.sort(array);
-		
+
 		return array;
 	} // END generateUniqueArray
-	
-	/** Helper Method
-	 * Array will check if passed number is inside of passed array
+
+	/**
+	 * Helper Method Array will check if passed number is inside of passed array
+	 * 
 	 * @param numToCheckFor The int to check the array for
-	 * @param arrayToCheck The array to check inside for the passed int.
-	 * @return a boolean value that is true if the array has the passed integer
-	 * and false if it does not
+	 * @param arrayToCheck  The array to check inside for the passed int.
+	 * @return a boolean value that is true if the array has the passed integer and
+	 *         false if it does not
 	 */
-	private static boolean arrayHasNumber (int numToCheckFor, int[] arrayToCheck) {
+	private static boolean arrayHasNumber(int numToCheckFor, int[] arrayToCheck) {
 		for (int i = 0; i < arrayToCheck.length; i++) {
 			if (numToCheckFor == arrayToCheck[i])
 				return true;
 		}
 		return false;
 	} // END arrayHasNumber
-	
-	
-/** 
- * Initializes a private class 2D array variable with 
- * the proper number of "quick-pick" lotto games.
- * @param games An integer that is the number of games that will be generated.
- */
+
+	/**
+	 * Initializes a private class 2D array variable with the proper number of
+	 * "quick-pick" lotto games.
+	 * 
+	 * @param games An integer that is the number of games that will be generated.
+	 */
 	private void init(int games) {
 		lottoArray = Arrays.copyOf(uniqueNumArray(), lottoArray.length);
 	} // END init
-	
 
 	/**
-	 * This method will call displayHeading() and displayFooter()
-	 * helper methods and in-between those two it will display the private member
-	 * lottoArray which contains the quick pick games that have been previously generated
+	 * This method will call displayHeading() and displayFooter() helper methods and
+	 * in-between those two it will display the private member lottoArray which
+	 * contains the quick pick games that have been previously generated
 	 */
 	public void displayTicket() {
-		
+
 		/**
-		 * display heading 
-		 * Game ticket should display a game specific heading including date/time.
+		 * display heading Game ticket should display a game specific heading including
+		 * date/time.
 		 * 
-		 * for i in gameCount 
-		 * 		generate selectionCount number of unique random selections in ascending order
-		 * 		 * Each game will be numbered/indexed - 1,2,3 ... n
-		 *      game numbers will be evenly spaced/formatted and single digit nums will be padded
-		 *      with a leading 0.
-		 *        		
-		 * display footer 
+		 * for i in gameCount generate selectionCount number of unique random selections
+		 * in ascending order * Each game will be numbered/indexed - 1,2,3 ... n game
+		 * numbers will be evenly spaced/formatted and single digit nums will be padded
+		 * with a leading 0.
+		 * 
+		 * display footer
 		 */
 
-		displayHeading(); 
-		
+		displayHeading();
+
 		/**
-		 * Display selected numbers 
+		 * Display selected numbers
 		 */
 		// Iterate through each row, which is a single lotto QP game
 		// Display each game number as "i+1".
 		// Display each element of the rows, zero padded
 		for (int i = 0; i < lottoArray.length; i++) {
-			System.out.printf("(%d)   ", i+1);
+			System.out.printf("(%d)   ", i + 1);
 			for (int j = 0; j < lottoArray[i].length; j++) {
 				System.out.printf(" %02d ", lottoArray[i][j]);
 			} // END inner for-loop
 			System.out.println();
 		} // END outer for-loop
-		
-		displayFooter(); 
-		
+
+		displayFooter();
+
 		return;
 	} // END displayTicket
-	
+
 	/**
 	 * Displays the heading of the Lotto ticket.
 	 */
 	protected void displayHeading() {
-	 System.out.println("-----------------------------------");
-	 System.out.println("-------------- " + GAME_NAME + " --------------");
-	 Date today = new Date();
-	 System.out.printf("    %s", today);
-	 System.out.println();
-	 System.out.println();
+		System.out.println("-----------------------------------");
+		System.out.println("-------------- " + GAME_NAME + " --------------");
+		Date today = new Date();
+		System.out.printf("    %s", today);
+		System.out.println();
+		System.out.println();
 	} // END displayHeading
-	
+
 	/**
-	 * Displays the footer of the lottery ticket
-	 * which includes the odds of winning.
+	 * Displays the footer of the lottery ticket which includes the odds of winning.
 	 */
 	protected void displayFooter() {
-		 BigInteger odds = new BigInteger("1");
-		 odds = calculateOdds();
-		 
+		BigInteger odds = new BigInteger("1");
+		odds = calculateOdds();
+
 		System.out.println();
-		 System.out.printf("Odds of Winning: 1 in %,d\n", odds);
-		 System.out.println("--------- (c) " + STORE_NAME + " ----------");
-		 System.out.println("-----------------------------------");
+		System.out.printf("Odds of Winning: 1 in %,d\n", odds);
+		System.out.println("--------- (c) " + STORE_NAME + " ----------");
+		System.out.println("-----------------------------------");
 	} // END displayFooter
-	
-	
+
 	/**
-	 *  This method executes the following algorithm to get the odds of winning a quick pick lottery game:
-	 *  SELECTION_POOL_SIZE! / SELECTION_COUNT! * (SELECTION_POOL_SIZE - SELECTION_COUNT)!
-	 *  
-	 *  We use factorial for the selection pool size because you have a 1 in selection_pool_size
-	 *  chance of matching a number. We have to divide by the order of the SELECTION_COUNT! 
-	 *  because we must take into account that it doesn't matter in which order the numbers are drawn to win.
-	 *   there are, in this case, 6! = 720 different ways they could be drawn. Then you multiply by
-	 *   the number of alternatives - the number of choices. This is called the combination function.
-	 *  
-	 * @return A BigInteger that is the result of calculating the odds of winning a quick pick
-	 * lottery ticket.
+	 * This method executes the following algorithm to get the odds of winning a
+	 * quick pick lottery game: SELECTION_POOL_SIZE! / SELECTION_COUNT! *
+	 * (SELECTION_POOL_SIZE - SELECTION_COUNT)!
+	 * 
+	 * We use factorial for the selection pool size because you have a 1 in
+	 * selection_pool_size chance of matching a number. We have to divide by the
+	 * order of the SELECTION_COUNT! because we must take into account that it
+	 * doesn't matter in which order the numbers are drawn to win. there are, in
+	 * this case, 6! = 720 different ways they could be drawn. Then you multiply by
+	 * the number of alternatives - the number of choices. This is called the
+	 * combination function.
+	 * 
+	 * @return A BigInteger that is the result of calculating the odds of winning a
+	 *         quick pick lottery ticket.
 	 */
 	private BigInteger calculateOdds() {
 		BigInteger odds = new BigInteger("1");
-		odds =  factorial(SELECTION_POOL_SIZE).divide( // start of bottom of division
-				factorial(SELECTION_COUNT).multiply(
-						factorial(SELECTION_POOL_SIZE - SELECTION_COUNT)
-						) // end bottom multiply
-				); // end division of top and bottom
+		odds = factorial(SELECTION_POOL_SIZE).divide( // start of denominator
+				factorial(SELECTION_COUNT).multiply(factorial(SELECTION_POOL_SIZE - SELECTION_COUNT)) // end denominator
+		); // end division of top and bottom
 		return odds;
 	} // END calculateOdds
-		
+
 	/**
 	 * Calculates a factorial of a given number with BigInteger
+	 * 
 	 * @param num The number we want the factorial of.
 	 * @return The result of the factorial in BigInteger form.
 	 */
@@ -206,25 +204,26 @@ public class LottoQuickPicker {
 		}
 		return sum;
 	} // END factorial
-  
 
 	/**
 	 * @param args String passed from the command line
 	 */
 	public static void main(String[] args) {
-		// takes an optional command line parameter specifying number of QP games to be generated
-		//  By default, generate 1  
-		int numberOfGames  = DEFAULT_GAME_COUNT; 
-	
-		if(args.length > 0) {  // if user provided an arg, assume it to be a game count
-			numberOfGames = Integer.parseInt(args[0]);  // [0] is the 1st element!
+		// takes an optional command line parameter specifying number of QP games to be
+		// generated
+		// By default, generate 1
+		int numberOfGames = DEFAULT_GAME_COUNT;
+
+		if (args.length > 0) { // if user provided an arg, assume it to be a game count
+			numberOfGames = Integer.parseInt(args[0]); // [0] is the 1st element!
 			GAME_COUNT = numberOfGames;
 		} // END if
-		//String input = JOptionPane.showInputDialog("How many games would you like to play?");
-		
+			// String input = JOptionPane.showInputDialog("How many games would you like to
+			// play?");
+
 		LottoQuickPicker lotto = new LottoQuickPicker(numberOfGames);
-		 
-		lotto.displayTicket(); 
+
+		lotto.displayTicket();
 	} // END main
 
 } // END LottoQuickPicker class
